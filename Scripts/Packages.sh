3c560f1549512c98a59c8d76dd4934eb64646c16
jptl123456 <153810397+jptl123456@users.noreply.github.com>
2026-01-09 17:08:37 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -2,42 +2,42 @@
 
 #安装和更新软件包
 UPDATE_PACKAGE() {
-	local PKG_NAME=$1
-	local PKG_REPO=$2
-	local PKG_BRANCH=$3
-	local PKG_SPECIAL=$4
-	local PKG_LIST=("$PKG_NAME" $5)  # 第5个参数为自定义名称列表
-	local REPO_NAME=${PKG_REPO#*/}
+local PKG_NAME=$1
+local PKG_REPO=$2
+local PKG_BRANCH=$3
+local PKG_SPECIAL=$4
+local PKG_LIST=("$PKG_NAME" $5)  # 第5个参数为自定义名称列表
+local REPO_NAME=${PKG_REPO#*/}
 
-	echo " "
+echo " "
 
-	# 删除本地可能存在的不同名称的软件包
-	for NAME in "${PKG_LIST[@]}"; do
-		# 查找匹配的目录
-		echo "Search directory: $NAME"
-		local FOUND_DIRS=$(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$NAME*" 2>/dev/null)
+# 删除本地可能存在的不同名称的软件包
+for NAME in "${PKG_LIST[@]}"; do
+# 查找匹配的目录
+echo "Search directory: $NAME"
+local FOUND_DIRS=$(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$NAME*" 2>/dev/null)
 
-		# 删除找到的目录
-		if [ -n "$FOUND_DIRS" ]; then
-			while read -r DIR; do
-				rm -rf "$DIR"
-				echo "Delete directory: $DIR"
-			done <<< "$FOUND_DIRS"
-		else
-			echo "Not fonud directory: $NAME"
-		fi
-	done
+# 删除找到的目录
+if [ -n "$FOUND_DIRS" ]; then
+while read -r DIR; do
+rm -rf "$DIR"
+echo "Delete directory: $DIR"
+done <<< "$FOUND_DIRS"
+else
+echo "Not found directory: $NAME"
+fi
+done
 
-	# 克隆 GitHub 仓库
-	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
+# 克隆 GitHub 仓库
+git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
 
-	# 处理克隆的仓库
-	if [[ "$PKG_SPECIAL" == "pkg" ]]; then
-		find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune -exec cp -rf {} ./ \;
-		rm -rf ./$REPO_NAME/
-	elif [[ "$PKG_SPECIAL" == "name" ]]; then
-		mv -f $REPO_NAME $PKG_NAME
-	fi
+# 处理克隆的仓库
+if [[ "$PKG_SPECIAL" == "pkg" ]]; then
+find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune -exec cp -rf {} ./ \;
+rm -rf ./$REPO_NAME/
+elif [[ "$PKG_SPECIAL" == "name" ]]; then
+mv -f $REPO_NAME $PKG_NAME
+fi
 }
 
 # ====================================================================
@@ -55,6 +55,11 @@ UPDATE_PACKAGE "luci-app-wrtbwmon" "brvphoenix/luci-app-wrtbwmon" "master"
 echo "添加 lucky..."
 UPDATE_PACKAGE "lucky" "gdy666/lucky" "main"
 UPDATE_PACKAGE "luci-app-lucky" "gdy666/luci-app-lucky" "main"
+
+# 3. 添加 rtp2httpd (已补充界面)
+echo "添加 rtp2httpd..."
+UPDATE_PACKAGE "rtp2httpd" "stackia/rtp2httpd" "master"
+UPDATE_PACKAGE "luci-app-rtp2httpd" "stackia/luci-app-rtp2httpd" "master"
 
 echo "缺失包添加完成！"
 
@@ -106,43 +111,43 @@ echo "其他包添加完成！"
 
 #更新软件包版本
 UPDATE_VERSION() {
-	local PKG_NAME=$1
-	local PKG_MARK=${2:-false}
-	local PKG_FILES=$(find ./ ../feeds/packages/ -maxdepth 3 -type f -wholename "*/$PKG_NAME/Makefile")
+local PKG_NAME=$1
+local PKG_MARK=${2:-false}
+local PKG_FILES=$(find ./ ../feeds/packages/ -maxdepth 3 -type f -wholename "*/$PKG_NAME/Makefile")
 
-	if [ -z "$PKG_FILES" ]; then
-		echo "$PKG_NAME not found!"
-		return
-	fi
+if [ -z "$PKG_FILES" ]; then
+echo "$PKG_NAME not found!"
+return
+fi
 
-	echo -e "\n$PKG_NAME version update has started!"
+echo -e "\n$PKG_NAME version update has started!"
 
-	for PKG_FILE in $PKG_FILES; do
-		local PKG_REPO=$(grep -Po "PKG_SOURCE_URL:=https://.*github.com/\K[^/]+/[^/]+(?=.*)" $PKG_FILE)
-		local PKG_TAG=$(curl -sL "https://api.github.com/repos/$PKG_REPO/releases" | jq -r "map(select(.prerelease == $PKG_MARK)) | first | .tag_name")
+for PKG_FILE in $PKG_FILES; do
+local PKG_REPO=$(grep -Po "PKG_SOURCE_URL:=https://.*github.com/\K[^/]+/[^/]+(?=.*)" $PKG_FILE)
+local PKG_TAG=$(curl -sL "https://api.github.com/repos/$PKG_REPO/releases" | jq -r "map(select(.prerelease == $PKG_MARK)) | first | .tag_name")
 
-		local OLD_VER=$(grep -Po "PKG_VERSION:=\K.*" "$PKG_FILE")
-		local OLD_URL=$(grep -Po "PKG_SOURCE_URL:=\K.*" "$PKG_FILE")
-		local OLD_FILE=$(grep -Po "PKG_SOURCE:=\K.*" "$PKG_FILE")
-		local OLD_HASH=$(grep -Po "PKG_HASH:=\K.*" "$PKG_FILE")
+local OLD_VER=$(grep -Po "PKG_VERSION:=\K.*" "$PKG_FILE")
+local OLD_URL=$(grep -Po "PKG_SOURCE_URL:=\K.*" "$PKG_FILE")
+local OLD_FILE=$(grep -Po "PKG_SOURCE:=\K.*" "$PKG_FILE")
+local OLD_HASH=$(grep -Po "PKG_HASH:=\K.*" "$PKG_FILE")
 
-		local PKG_URL=$([[ "$OLD_URL" == *"releases"* ]] && echo "${OLD_URL%/}/$OLD_FILE" || echo "${OLD_URL%/}")
+local PKG_URL=$([[ "$OLD_URL" == *"releases"* ]] && echo "${OLD_URL%/}/$OLD_FILE" || echo "${OLD_URL%/}")
 
-		local NEW_VER=$(echo $PKG_TAG | sed -E 's/[^0-9]+/\./g; s/^\.|\.$//g')
-		local NEW_URL=$(echo $PKG_URL | sed "s/\$(PKG_VERSION)/$NEW_VER/g; s/\$(PKG_NAME)/$PKG_NAME/g")
-		local NEW_HASH=$(curl -sL "$NEW_URL" | sha256sum | cut -d ' ' -f 1)
+local NEW_VER=$(echo $PKG_TAG | sed -E 's/[^0-9]+/\./g; s/^\.|\.$//g')
+local NEW_URL=$(echo $PKG_URL | sed "s/\$(PKG_VERSION)/$NEW_VER/g; s/\$(PKG_NAME)/$PKG_NAME/g")
+local NEW_HASH=$(curl -sL "$NEW_URL" | sha256sum | cut -d ' ' -f 1)
 
-		echo "old version: $OLD_VER $OLD_HASH"
-		echo "new version: $NEW_VER $NEW_HASH"
+echo "old version: $OLD_VER $OLD_HASH"
+echo "new version: $NEW_VER $NEW_HASH"
 
-		if [[ "$NEW_VER" =~ ^[0-9].* ]] && dpkg --compare-versions "$OLD_VER" lt "$NEW_VER"; then
-			sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$NEW_VER/g" "$PKG_FILE"
-			sed -i "s/PKG_HASH:=.*/PKG_HASH:=$NEW_HASH/g" "$PKG_FILE"
-			echo "$PKG_FILE version has been updated!"
-		else
-			echo "$PKG_FILE version is already the latest!"
-		fi
-	done
+if [[ "$NEW_VER" =～ ^[0-9].* ]] && dpkg --compare-versions "$OLD_VER" lt "$NEW_VER"; then
+sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$NEW_VER/g" "$PKG_FILE"
+sed -i "s/PKG_HASH:=.*/PKG_HASH:=$NEW_HASH/g" "$PKG_FILE"
+echo "$PKG_FILE version has been updated!"
+else
+echo "$PKG_FILE version is already the latest!"
+fi
+done
 }
 
 echo "开始更新软件包版本..."
@@ -176,6 +181,8 @@ check_package "wrtbwmon"
 check_package "luci-app-wrtbwmon"
 check_package "lucky"
 check_package "luci-app-lucky"
+check_package "rtp2httpd"
+check_package "luci-app-rtp2httpd"
 
 echo "=========================================="
 echo "Packages.sh 脚本执行完成！"
